41827e523ed334d86323c5ef8ef7c8602c33bce1
Kewalytz <155101903+Kewalytz@users.noreply.github.com>
2024-12-22 00:47:04 +0530

    Create Apps.js

---
+++ Apps.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
+import AdminDashboard from './components/AdminDashboard';
+import UserDashboard from './components/UserDashboard';
+
+const App = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="/admin" element={<AdminDashboard />} />
+                <Route path="/user" element={<UserDashboard />} />
+            </Routes>
+        </Router>
+    );
+};
+
+export default App;
